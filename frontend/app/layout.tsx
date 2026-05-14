c1413eef254f4765e5b6c1b4d997c7c576f65938
mangalgithub <127006680+mangalgithub@users.noreply.github.com>
2026-05-14 22:31:54 +0530

    feature/added speed-insights in vercel

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,7 @@
 
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { DashboardProvider } from "@/context/DashboardContext";
 import Sidebar from "@/components/nav/Sidebar";
 import MobileNav from "@/components/nav/MobileNav";
@@ -24,6 +25,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           {!isAuthPage && <MobileNav />}
         </DashboardProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
